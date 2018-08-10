1aae6f7566da38be059bf5d2e5876df29c00b7d3
Cassandra <cassandra.donatelli@gmail.com>
2018-08-10 15:48:07 -0700

    Added comments to code
    Specifically how to use "extract coordinates"

--- extractCoordinates.m
+++ extractCoordinates.m
@@ -1,12 +1,25 @@
+% To Run this code:
+% 1) put all fcsv files in the same folder
+% 2) Make that folder your working directory (navigate to it in the bar
+%    above the script editor window)
+% 3) Run the ode by clicking "Run" in the editor tab
 
 names = dir('*.fcsv');
-
 TenPercPoints = {};
 
 for i = 1:length(names)
     filename = names(i).name;
-    species = filename(1:end-9);
-    perc = filename(end-8:end-7);
+    % edit this based on your file naming scheme
+    
+    % the end should be end-## where the ## is the number of characters
+    % following your species name. For example:
+    % Apurp001_20.fcsv <-- ## = 8
+    species = filename(1:end-8);
+    
+    % the end should be end-n1:end-n2 where the n's are the number of ch
+    % caracters surrounding your vert percent. For example:
+    % Apurp001_20.fcsv <-- n1 = 7; n2 = 5
+    perc = filename(end-6:end-5);
     
     %Read In File
     %filename = 'Z:\Donatelli\Elongate Fish Vert Mechanics\Raw data - ...
