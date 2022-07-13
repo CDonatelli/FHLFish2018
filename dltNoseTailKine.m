cac91916fbe7ee62ac8be940f2f7011d14180f86
Cassandra Donatelli <cassandra.donatelli@gmail.com>
2022-07-13 16:10:53 -0700

    fixed distance

--- dltNoseTailKine.m
+++ dltNoseTailKine.m
@@ -42,12 +42,15 @@ dataStruct = struct;
     
     tailAmps = abs(p*dataStruct.scale);
     
-    nose = [dataStruct.nosePts(1,1), dataStruct.nosePts(1,2); ...
-            dataStruct.nosePts(end,1), dataStruct.nosePts(end,2);];
-    distance = pdist(nose, 'euclidean');
-    distance = distance.*dataStruct.scale;
+%     nose = [dataStruct.nosePts(1,1), dataStruct.nosePts(1,2); ...
+%             dataStruct.nosePts(end,1), dataStruct.nosePts(end,2);];
+
+    distance = arclength(dataStruct.nosePts(:,1), dataStruct.nosePts(:,2));
+
+%     distance = pdist(nose, 'euclidean');
+    distance = distance.*dataStruct.scale/1000;
     %speed in m/s
-    dataStruct.swimmingSpeed = (distance/dataStruct.t(end))/1000;
+    dataStruct.swimmingSpeed = (distance/dataStruct.t(end));
     dataStruct.bendingFrequency = size(p,1)/2/dataStruct.t(end);
     dataStruct.bendingPeriod = 1/dataStruct.bendingFrequency;
     dataStruct.bendingStrideLength = distance/1000/(size(p,1)/2);
